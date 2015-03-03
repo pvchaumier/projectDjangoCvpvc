f8e92506614f0ff9208fbb635909880d6573aee1
Pierre-Victor Chaumier <chaumierpv@gmail.com>
2015-03-03 19:01:53 -0300

    mardown custom template tag

---
+++ cvpvcBlog/apps/cvpvc_core/templatetags/markdown_filter.py
@@ -0,0 +1,11 @@
+#-*- coding: utf-8 -*-
+
+from django import template
+import markdown
+ 
+register = template.Library()
+ 
+@register.filter
+def markdownify(text):
+    # safe_mode governs how the function handles raw HTML
+    return markdown.markdown(text, safe_mode='escape') 
