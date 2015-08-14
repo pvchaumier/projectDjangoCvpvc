8d6d162f1fa816e7f9a6b58b96ff74e5ebd54c0f
Pierre-Victor Chaumier <chaumierpv@gmail.com>
2015-08-14 12:33:54 -0300

    Adding logo

--- cvpvcBlog/apps/cvpvc_core/templatetags/markdown_filter.py
+++ cvpvcBlog/apps/cvpvc_core/templatetags/markdown_filter.py
@@ -8,4 +8,5 @@ register = template.Library()
 @register.filter
 def markdownify(text):
     # safe_mode governs how the function handles raw HTML
-    return markdown.markdown(text, safe_mode='escape') 
+    return markdown.markdown(text, safe_mode='escape')
+    
